32d47e4b20a8cae1b8f0dda5a2c5c93556e5bd65
El tom <tomas.garcia.pineiro@gmail.com>
2019-05-23 19:10:04 -0300

    backup

--- src/app/principal/categorias/categoria-a/categoria-a.page.ts
+++ src/app/principal/categorias/categoria-a/categoria-a.page.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit, ViewChild, Inject } from '@angular/core';
-import { NavController, Events } from '@ionic/angular';
-import { NativeAudio } from '@ionic-native/native-audio/ngx';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from '@ionic/angular';
 import { CategoriasLogicService } from 'src/app/services/categorias-logic/categorias-logic.service';
 import { Timer } from 'easytimer.js';
-import { UserDataService } from 'src/app/services/user-data/user-data.service';
-import { DOCUMENT } from '@angular/platform-browser';
-import { URL_ASSETS } from 'src/config/config';
-import { DownloadService, LocalStorageService, ToastService } from 'src/app/services/services.index';
+import { DownloadService, ToastService } from 'src/app/services/services.index';
 
 @Component({
     selector: 'app-categoria-a',
@@ -19,84 +15,47 @@ export class CategoriaAPage implements OnInit {
     @ViewChild('progress-bar') progress;
 
 
-    // variablesOperativas
-
-    categoria: string;
-    playState: boolean = false;
-
-    iconoFavorito: string = 'med-corazon-vacio';
-
     audio = new Audio();
-    indexPlay: number = 0;
-    url: string;
-    duracion: number;
-    timer = new Timer();
-    isPlaying: boolean = false;
-    showLoader: boolean = false;
 
-    favHabilitado: boolean = true;
+    // variablesOperativas
+    categoria = "categoria_a";
+    medias = [];
+
+    // Intervals
+    interval: any;
+    minutero: any = "00:00";
+
+    timer = new Timer();
 
     // progress bar
     progresoBar: number = 0;
-    minutero: any = "00:00"
-
-    // Intervals
-    intervalProgress: any;
-    minuteroInterval: any;
-    timeoutLoader: any;
-
     resumeAfterTouchStart: boolean;
 
-    medias = [];
-    progressBarElement: any;
-
-    isNewView = false;
-    loaderElement:any;
-
     downloadIconColor:string = "light";
+
     constructor(private navCtrl: NavController,
-        private categoriasLogic: CategoriasLogicService,
-        private userData: UserDataService,
-        private event: Events,
-        @Inject(DOCUMENT) private _document: Document,
+        public _CL: CategoriasLogicService,
         private downloadServ: DownloadService,
-        private toastServ: ToastService,
-        private localStorageServ: LocalStorageService) {
-        this.progressBarInteractionSubscriptions();
-        this.audioSubscriptions();
+        private toastServ: ToastService) {
+
     }
 
     async ngOnInit() {
-      this.categoria = "categoria_a";
-      this.processUrls(this.categoriasLogic.audiosCategoria);
-      this.initProgressBarAndLoader();
-      this.slideSubscriptions();
 
-      if(this.medias[0] != undefined){
-        this.initNewAudio(this.medias[0].url, 0);
-      }
 
-      let directory = this.downloadServ.getDeviceDirectory();
-
-      if(directory == null){
-        if(this.localStorageServ.localStorageObj[this.categoria] != undefined){
-          this.downloadIconColor = "success";
-        }else{
-          this.downloadIconColor = "light";
-        }
-      }else{
-        let responseDirectory = await this.downloadServ.getCategoriaDirectory(this.categoria, directory);
-        if(responseDirectory["found"]){
-          this.downloadIconColor = "success";
-          let filesArray:any = this.downloadServ.generateFilePathArray(this.medias, this.categoria);
-          let resultFiles = await this.downloadServ.switchCordovaAndDataDirectories(filesArray,this.categoria);
-          console.log("Resultado de files en categoria: " + resultFiles)
-        }else{
-          this.downloadIconColor = "light";
-        }
-      }
-
+      this._CL.progressBarInteractionSubscriptions(this.audio,
+                                                    this.interval,this.medias, this.timer, this.slides,
+                                                   this.resumeAfterTouchStart, function(minuteroCB){
+                                                     console.log(minuteroCB)
+                                                     this.minutero = minuteroCB;
+                                                   });
+      this._CL.processUrls(this.medias);
+      this._CL.initProgressBarAndLoader(this.medias);
+      this._CL.slideSubscriptions(this.slides,  this.interval, this.audio, this.medias, this.timer);
+      this._CL.init(this.medias, this.audio, this.downloadIconColor, this.categoria);
+      this._CL.audioSubscriptions(this.audio, this.medias, this.interval);
     }
+
 
     download(){
       if(this.downloadIconColor == "success"){
@@ -121,273 +80,36 @@ export class CategoriaAPage implements OnInit {
     }
 
 
-
-    processUrls(urls:Array<any>){
-      urls.filter((value)=>{
-        let ultimosCuatroChars = value.substr(value.length - 4);
-        if( ultimosCuatroChars == ".mp4" ||
-            ultimosCuatroChars == ".mp3"){
-              this.pushAudioToMainArray(value);
-            }
-      });
-    }
-
-    pushAudioToMainArray(url){
-      this.medias.push({
-        "url": URL_ASSETS + url,
-        "icon":"play",
-        "animateUp" :false,
-        "id_loader": Math.random(),
-        "id_minutero":Math.random(),
-        "id_progress":Math.random(),
-        "fadeIn":false
-      });
-      return;
-    }
-
-    initProgressBarAndLoader(){
-      let length = this.medias.length;
-      for (let i = 0; i < length; i ++){
-        setTimeout(()=>{
-          this._document.getElementById(this.medias[i].id_loader).style.visibility = "hidden";
-          this._document.getElementById(this.medias[i].id_minutero).style.visibility = "hidden";
-          this._document.getElementById(this.medias[i].id_progress).style.visibility = "hidden";
-        },100)
-      }
-    }
-
-    slideSubscriptions(){
-      this.slides.ionSlideDidChange.subscribe((data) => {
-
-          this.isNewView = true;
-
-          if (this.medias[this.indexPlay] != undefined) {
-
-              let indexSlide = this.slides.getActiveIndex();
-
-              // SE VA Y EL AUDIO ESTA CORRIENDO
-              if (indexSlide > this.medias.length - 1) {
-                  indexSlide = this.medias.length - 1;
-              };
-
-              if (!this.audio.paused) {
-                this.audio.src = "";
-                this.pausarAudio();
-              }
-          }
-      });
-    }
-
-    ocultarElementosDeUnAudio(){
-      this._document.getElementById(this.medias[this.indexPlay].id_loader).style.visibility = "hidden";
-      this._document.getElementById(this.medias[this.indexPlay].id_minutero).style.visibility = "hidden";
-      this._document.getElementById(this.medias[this.indexPlay].id_progress).style.visibility = "hidden";
-    }
-
     pausarAudio(){
-      clearInterval(this.intervalProgress);
-      clearInterval(this.minuteroInterval);
-      /*
-      this.nativeAudio.stop("audio").then((pause)=>{
-      console.log(pause)
-    })
-      */
-      this.audio.pause();
-      this.medias[this.indexPlay].icon = "play";
-      this.grabarSegundosMeditados();
+      this._CL.pausarAudio(this.interval, this.audio, this.medias, this.timer);
     }
 
     dismiss() {
         this.navCtrl.navigateRoot("/tabs/principal");
     }
 
+
+
     playPause(url, index) {
         // Puede ser una pausa o un play
-        this.minutero = this.categoriasLogic.minuteroFunction(this.audio.currentTime);
-        if (this.isPlaying) {
 
-            this.isPlaying = false;
-            this.pausarAudio();
+        this.minutero = this._CL.minuteroFunction(this.audio.currentTime);
+        this._CL.playPause(url, index, this.audio,
+                            this.interval,this.medias, this.timer,
+                           (interval, minuteroCB,progresoCB)=>{
+                             this.interval = interval;
+                             this.progresoBar = progresoCB;
+                             this.minutero = minuteroCB;
+                           });
 
-        } else {
-
-
-            this.timer.start();
-            this.medias[index].icon = "pause";
-            this.medias[index].animateUp = true;
-            setTimeout(()=>{
-              this._document.getElementById(this.medias[index].id_progress).style.visibility = "visible";
-              this._document.getElementById(this.medias[index].id_minutero).style.visibility = "visible";
-              this.medias[index].fadeIn = true;
-            },1500)
-
-            // Puede ser un url nuevo o uno ya corriendo
-
-            if (this.isNewView) {
-              console.log("Se inicializa un nuevo audio")
-              this.initNewAudio(url,index);
-            };
-            /*
-            this.nativeAudio.play("audio").then((play) => {
-            console.log(play);
-            this.isPlaying = true;
-            this.progresoIntervalFN();
-            this.minuteroIntervalFN();
-          })
-          .catch((err)=>{
-          console.log(err)
-        });
-            */
-            this.audio.play().then((play) => {
-              this.isPlaying = true;
-              this.progresoIntervalFN();
-              this.minuteroIntervalFN();
-            })
-            .catch((err)=>{
-              console.log(err)
-            });
-        }
     }
 
-    initNewAudio(url,index){
-      this.url = url;
-      this.indexPlay = index;
-      /*
-      this.nativeAudio.preloadSimple("audio",url).then((nativeAudio)=>{
-      console.log(nativeAudio);
-    })
-      */
-      this.audio.src = url;
-      this.audio.load();
-      return;
-    }
-
-   minuteroIntervalFN(){
-     this.minuteroInterval = setInterval(() => {
-         this.minutero = this.categoriasLogic.minuteroFunction(this.audio.currentTime);
-     }, 1000);
-   }
-
-
-    progresoIntervalFN() {
-        this.duracion = this.audio.duration;
-        this.intervalProgress = setInterval(() => {
-            this.progresoBar = (this.audio.currentTime / this.duracion) * 100;
-        }, 1000);
-    }
-
-
-    cambiarProgresoAudio(progressPercentage, resume) {
-      /*
-        this.audio.currentTime = Math.round(progressPercentage*100)/100 * this.audio.duration;
-        this.audio.load();
-        this.audio.currentTime = 100;
-        console.log("cambio de progres" , this.audio.currentTime)
-        */
-        this.audio.play();
-        this.audio.addEventListener('canplaythrough', function() {
-          console.log("can play")
-          this.currentTime = 100;
-          this.play();
-        });
-        /*
-        if (resume) {
-            this.playPause(this.url, this.indexPlay);
-        }
-        */
-    }
-
-    grabarSegundosMeditados() {
-
-        // Envío la data cuando se va de la página o después de 5 minutos de inactividad marcada por la play function
-        return new Promise((resolve, reject) => {
-            let segundosMeditadosUser = this.userData.userData.segundos_meditados;
-            let segundosMeditadosSesion = this.timer.getTimeValues().seconds;
-            this.timer.stop();
-            let segundosTotales = segundosMeditadosUser + segundosMeditadosSesion;
-            this.userData.userData.segundos_meditados = segundosTotales;
-
-            this.userData.updateUserData({
-                "segundos_meditados": segundosTotales
-            }).subscribe(() => {
-                console.log("Segundos meditados: " + segundosTotales);
-                resolve();
-            });
-        })
-    }
 
 
     ngOnDestroy() {
-        this.grabarSegundosMeditados();
-        this.audio.pause();
-        clearInterval(this.minuteroInterval);
-        clearInterval(this.intervalProgress);
+        // Me tengo que desubscribir de los eventos
+        this._CL.destroyComponent( this.interval, this.audio);
+        this._CL.grabarSegundosMeditados(this.timer);
     }
 
-    audioSubscriptions() {
-
-
-        this.audio.addEventListener("progress", () => {
-
-            // LOADER
-
-            if (this.isPlaying) {
-                this._document.getElementById(this.medias[this.indexPlay].id_loader).style.visibility = "visible";
-                this.timeoutLoader = setTimeout(() => {
-                    this._document.getElementById(this.medias[this.indexPlay].id_loader).style.visibility = "hidden";
-                }, 1000);
-            }
-        });
-
-        this.audio.addEventListener("ended", () => {
-
-            this.medias[this.indexPlay].icon = "med-play";
-
-            // ABRIR PAGINAS DE NOTAS DE USUARIO !
-
-            clearInterval(this.intervalProgress);
-        });
-
-    }
-
-    progressBarInteractionSubscriptions() {
-
-        // LOGICA COMPONENTE PROGRESS BAR
-
-        this.event.subscribe("swipes", (lock) => {
-            if (lock) {
-                this.slides.lockSwipes(true)
-            } else {
-                this.slides.lockSwipes(false)
-            }
-        })
-
-        this.event.subscribe("touchend", (objTouchEnd: {
-            progressPercentage
-        }) => {
-            this.cambiarProgresoAudio(objTouchEnd.progressPercentage, this.resumeAfterTouchStart);
-        })
-
-        this.event.subscribe("touchstart", () => {
-            if (this.isPlaying) {
-                this.playPause(this.url, this.indexPlay);
-                this.resumeAfterTouchStart = true;
-            } else {
-                this.resumeAfterTouchStart = false;
-            }
-        });
-    }
-
-}
-
-interface objMeditacion {
-    id: number,
-    url: string,
-    orden: number,
-    icon: string,
-    showBar: boolean,
-    escuchada: boolean,
-    tick: boolean,
-    fav: boolean,
-    downloaded: string
 }
--- src/app/services/categorias-logic/categorias-logic.service.ts
+++ src/app/services/categorias-logic/categorias-logic.service.ts
@@ -1,17 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { HttpService } from '../http.service';
 import { UserDataService } from '../user-data/user-data.service';
-import { URL_SERVICES } from 'src/config/config';
+import { URL_SERVICES, URL_ASSETS } from 'src/config/config';
+import { Events } from '@ionic/angular';
+import { DOCUMENT } from '@angular/platform-browser';
+import { DownloadService } from '../download/download.service';
+import { LocalStorageService } from '../local-storage/local-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriasLogicService {
+
   token: string;
   audiosCategoria = [];
+  url = "";
+  index = 0;
+
 
   constructor(private http:HttpService,
-              private userDataServ: UserDataService) {
+              private userDataServ: UserDataService,
+              private event: Events,
+              @Inject(DOCUMENT) private _document: Document,
+              private userData: UserDataService,
+              private downloadServ: DownloadService,
+              private localStorageServ: LocalStorageService) {
                 this.token = this.userDataServ.token;
               }
 
@@ -83,6 +96,227 @@ export class CategoriasLogicService {
     return this.http.httpGet(url);
   }
 
+  progressBarInteractionSubscriptions(audio,
+                                      intervalInt,
+                                      medias, timer, slides, resumeAfterTouchStart, callbackMinutero){
+
+    this.event.subscribe("swipes", (lock) => {
+        if (lock) {
+            slides.lockSwipes(true)
+        } else {
+            slides.lockSwipes(false)
+        }
+    });
+
+    this.event.subscribe("touchend", (objTouchEnd: {
+        progressPercentage
+    }) => {
+        this.cambiarProgresoAudio(objTouchEnd.progressPercentage, resumeAfterTouchStart, audio);
+    });
+
+    this.event.subscribe("touchstart", () => {
+        if (audio.paused == false) {
+
+          this.playPause(this.url, this.index, audio,
+                    intervalInt,
+                    medias, timer,
+                    callbackMinutero);
+
+            resumeAfterTouchStart = true;
+        } else {
+            resumeAfterTouchStart = false;
+        }
+    });
+  }
 
 
+
+  cambiarProgresoAudio(progressPercentage, resume, audio = new Audio()) {
+
+      setTimeout(()=>{
+        audio.play();
+      }, 2000)
+  }
+
+
+  destroyComponent(progresInt, audio){
+    audio.pause();
+    this.clearIntervals(progresInt);
+    this.event.unsubscribe("swipes");
+    this.event.unsubscribe("touchend");
+    this.event.unsubscribe("touchstart");
+  }
+
+  clearIntervals(progresInt){
+    clearInterval(progresInt);
+    return;
+  }
+
+
+  processUrls(medias){
+    let validMimes = [".mp4" ,".mp3"];
+    this.audiosCategoria = this.audiosCategoria.filter((value)=> validMimes.includes(value.substr(value.length - 4)));
+    this.audiosCategoria.filter((value)=>{
+      medias.push({
+        "url": URL_ASSETS + value,
+        "icon":"play",
+        "animateUp" :false,
+        "id_loader": Math.random(),
+        "id_minutero":Math.random(),
+        "id_progress":Math.random(),
+        "fadeIn":false
+      });
+    });
+  }
+
+  initProgressBarAndLoader(medias){
+    let length = medias.length;
+    for (let i = 0; i < length; i ++){
+      setTimeout(()=>{
+        this._document.getElementById(medias[i].id_loader).style.visibility = "hidden";
+        this._document.getElementById(medias[i].id_minutero).style.visibility = "hidden";
+        this._document.getElementById(medias[i].id_progress).style.visibility = "hidden";
+      },100)
+    }
+  }
+
+  slideSubscriptions(slides, intervalInt, audio, medias, timer){
+    slides.ionSlideDidChange.subscribe(() => {
+
+        if (medias[this.index] != undefined) {
+            if (!audio.paused) {
+              audio.src = "";
+              this.pausarAudio(intervalInt, audio, medias, timer);
+            }
+        }
+    });
+  }
+
+  pausarAudio(intervalInt, audio, medias, timer){
+    this.clearIntervals(intervalInt);
+    audio.pause();
+    medias[this.index].icon = "play";
+    this.grabarSegundosMeditados(timer);
+  }
+
+  grabarSegundosMeditados(timer) {
+
+      // Envío la data cuando se va de la página o después de 5 minutos de inactividad marcada por la play function
+      return new Promise((resolve) => {
+          let segundosMeditadosUser = this.userData.userData.segundos_meditados;
+          let segundosMeditadosSesion = timer.getTimeValues().seconds;
+          timer.stop();
+          let segundosTotales = segundosMeditadosUser + segundosMeditadosSesion;
+          this.userData.userData.segundos_meditados = segundosTotales;
+
+          this.userData.updateUserData({
+              "segundos_meditados": segundosTotales
+          }).subscribe(() => {
+              console.log("Segundos meditados: " + segundosTotales);
+              resolve();
+          });
+      })
+  }
+
+  playPause(url, index, audio,
+            intervalInt,
+            medias, timer,
+            callbackMinutero) {
+      this._document.getElementById("play-button").classList.add("moveUp");
+
+      if (audio.paused == false) {
+
+          this.pausarAudio(intervalInt, audio, medias, timer);
+
+      } else {
+
+          medias[index].animateUp = true;
+          timer.start();
+          medias[index].icon = "pause";
+          if(this._document.getElementById(medias[index].id_progress).style.visibility == "hidden"){
+            setTimeout(()=>{
+              this._document.getElementById(medias[index].id_progress).style.visibility = "visible";
+              this._document.getElementById(medias[index].id_minutero).style.visibility = "visible";
+              medias[index].fadeIn = true;
+            },1500);
+          }
+
+          if(this.index != index){
+            this.initNewAudio(url,index, audio);
+          }
+
+          audio.play().then(() => {
+            this.intervalsFN(audio, function(interval, minuteroCB, progresoCB){
+              callbackMinutero(interval, minuteroCB, progresoCB);
+            });
+          })
+          .catch((err)=>{
+            console.log(err)
+          });
+      }
+  }
+  async init(medias, audio, downloadIconColor, categoria){
+
+      if(medias[0] != undefined){
+        this.initNewAudio(medias[0].url, 0, audio);
+      }
+
+      let directory = this.downloadServ.getDeviceDirectory();
+
+      if(directory == null){
+        if(this.localStorageServ.localStorageObj[categoria] != undefined){
+          downloadIconColor = "success";
+        }else{
+          downloadIconColor = "light";
+        }
+      }else{
+        let responseDirectory = await this.downloadServ.getCategoriaDirectory(categoria, directory);
+        if(responseDirectory["found"]){
+          downloadIconColor = "success";
+          let filesArray:any = this.downloadServ.generateFilePathArray(medias, categoria);
+          let resultFiles = await this.downloadServ.switchCordovaAndDataDirectories(filesArray,categoria);
+          console.log("Resultado de files en categoria: " + resultFiles)
+        }else{
+          downloadIconColor = "light";
+        }
+      }
+  }
+
+  initNewAudio(url,index, audio){
+    this.url = url;
+    this.index = index;
+    audio.src = url;
+    audio.load();
+    return;
+  }
+
+  intervalsFN(audio, callback){
+    let interval = setInterval(() =>
+    {
+        console.log("Interval cycle")
+        let minutero = this.minuteroFunction(audio.currentTime);
+        let progreso = (audio.currentTime / audio.duration) * 100;
+        callback(interval, minutero, progreso);
+    }, 1000);
+  }
+
+
+   audioSubscriptions(audio, media, intervalsFN){
+     audio.addEventListener("progress", () => {
+
+         // LOADER
+         console.log("progress")
+         if (audio.paused == false) {
+             this._document.getElementById(media[this.index].id_loader).style.visibility = "visible";
+             setTimeout(() => {
+                 this._document.getElementById(media[this.index].id_loader).style.visibility = "hidden";
+             }, 1000);
+         }
+     });
+
+     audio.addEventListener("ended", () => {
+         media[this.index].icon = "med-play";
+         clearInterval(intervalsFN);
+     });
+   }
 }
